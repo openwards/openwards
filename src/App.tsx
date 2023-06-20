ccfc5a939899b8f440ab858f79678aceafe0cd5a
camilo-camargo <camilocamargo49@gmail.com>
2023-06-20 06:48:56 -0500

    Change app location.

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import {Text, SafeAreaView} from 'react-native'
+export default function App() {
+  return (
+    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>OpenWards</Text>
+    </SafeAreaView>
+  );
+}
