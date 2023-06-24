09ed05a5872607d66c1f25113fb4fa7af55c3b06
camilo-camargo <camilocamargo49@gmail.com>
2023-06-23 19:01:05 -0500

    Add Firebase connection. - Add google services; - Add firebase auth package;

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import {Text, SafeAreaView} from 'react-native'
+import { useEffect, useState } from 'react';
+import {Text, SafeAreaView, View, TextInput, Button} from 'react-native'
+import auth from '@react-native-firebase/auth';
+
 export default function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  const [email, setEmail] = useState<String>();
+  const [password, setPassword] = useState<String>();
+
+  function onAuthStateChange(user: any){
+      setUser(user);
+    if(initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChange);
+    return subscriber;
+  }, []); 
+
+  if(initializing) return null;
+
+  if(!user){
+    return (
+      <View>
+        <TextInput onChangeText={setPassword}>Welcome</TextInput>
+        <TextInput onChangeText={setEmail}>Email</TextInput>
+        <Button title='Sign in'/>
+      </View>
+    );
+  }
+
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>OpenWards</Text>
-    </SafeAreaView>
+    <View>Welcome {user}</View>
   );
 }
