5d6f3c36fadaaa8fb9bd693953d7a85e149d47e4
Camilo Andres Camargo Castañeda <camilocamargo49@gmail.com>
2023-06-27 18:55:25 -0500

    Revert "Feature/emulator startup script"

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {Text, SafeAreaView} from 'react-native'
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color:"red"}}>OpenWards</Text>
+      <Text>OpenWards</Text>
     </SafeAreaView>
   );
 }
