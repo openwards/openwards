45dd2874fb5175d973ffbe709593f7ec9b08f209
CamiloArias12 <ariaspira13@gmail.com>
2023-06-26 23:05:30 -0500

    script

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {Text, SafeAreaView} from 'react-native'
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>OpenWards</Text>
+      <Text style={{color:"red"}}>OpenWards</Text>
     </SafeAreaView>
   );
 }
