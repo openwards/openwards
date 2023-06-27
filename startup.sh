72b140f1573094d7e740d9b12d2ff58833dc0be9
camilo-camargo <camilocamargo49@gmail.com>
2023-06-26 20:50:03 -0500

    Add simple emulator start with scrcpy

---
+++ startup.sh
@@ -0,0 +1,11 @@
+PATH_EMULATOR="$HOME/Android/Sdk/emulator"
+PATH_EXE_EMULATOR="$PATH_EMULATOR/emulator"
+
+LIST_EMULATORS=`$PATH_EXE_EMULATOR  -list-avds | head -n 1`
+NAME_EMULATOR=$LIST_EMULATORS
+
+`$PATH_EXE_EMULATOR -avd $NAME_EMULATOR -no-window & scrcpy`
+
+
+
+
